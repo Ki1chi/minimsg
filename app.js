80948000f32cb7d5c40c90a2328482b056e643ec
Nori <nori.terada9@gmail.com>
2024-08-22 18:24:05 -0400

    -added views

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs")
 
 
 
-app.use('/index', indexRouter);
+app.use('/', indexRouter);
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+  ];
 
 router.get('/', (req,res) => {
     res.send("Hello world")
 })
 
-router.get('/about', (req, res) => {
-    res.send('hello from about')
+router.get('/new', (req, res) => {
+    res.send('hello from new')
 })
 
 
